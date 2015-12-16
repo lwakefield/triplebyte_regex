7204088d62aee72af0b42f5d0aa19641a13d047b
Lawrence Wakefield <lawrence@iamlawrence.me>
2015-12-17 09:07:56 +1100

    fairly robust quantifiers

--- query.py
+++ query.py
@@ -9,6 +9,37 @@ class Query(object):
         self.suffix = ''
 
     def match(self, text, index=0):
+        if '*' in self.suffix:
+            return self.match_zero_or_more(text, index)
+        elif '+' in self.suffix:
+            return self.match_one_or_more(text, index)
+        elif '?' in self.suffix:
+            return self.match_zero_or_one(text, index)
+        return self.match_all_sub_queries(text, index)
+
+    def match_zero_or_one(self, text, index=0):
+        match = self.match_all_sub_queries(text, index)
+        if match:
+            return match
+        return Match(index, '')
+
+    def match_one_or_more(self, text, index=0):
+        match = self.match_all_sub_queries(text, index)
+        if match:
+            return self.match_zero_or_more(text, index)
+        return None
+
+    def match_zero_or_more(self, text, index=0):
+        accumulated_match = Match(index, '')
+        while True:
+            start_index = index + len(accumulated_match.text)
+            match = self.match_all_sub_queries(text, start_index)
+            if match and match.index == start_index:
+                accumulated_match = Match.join_matches([accumulated_match, match])
+            else: break
+        return accumulated_match
+
+    def match_all_sub_queries(self, text, index=0):
         matches = []
         for query in self.queries:
             if type(query) is str:
@@ -28,10 +59,45 @@ class Query(object):
                 else: return None
         return Match.join_matches(matches)
 
-class OrQuery(object):
+class OrQuery(Query):
 
     def match(self, text, index=0):
         query = self.queries[0]
+        for i in range(index, len(text)):
+            match = None
+            if '*' in self.suffix:
+                match = self.match_zero_or_more(text, i)
+            elif '+' in self.suffix:
+                match = self.match_one_or_more(text, i)
+            elif '?' in self.suffix:
+                match = self.match_zero_or_one(text, i)
+            elif text[index] in query:
+                match = Match(i, text[i])
+            if match:
+                return match
+        return None
+
+    def match_zero_or_one(self, text, index=0):
+        query = self.queries[0]
         if text[index] in query:
             return Match(index, text[index])
+        return Match(index, '')
+
+    def match_one_or_more(self, text, index=0):
+        query = self.queries[0]
+        match_text = ''
+        if text[index] in query:
+            return self.match_zero_or_more(text, index)
         return None
+
+    def match_zero_or_more(self, text, index=0):
+        query = self.queries[0]
+        match_text = ''
+        for val in text[index:]:
+            if val in query:
+                match_text += val
+            else: 
+                break
+
+        return Match(index, match_text)
+
